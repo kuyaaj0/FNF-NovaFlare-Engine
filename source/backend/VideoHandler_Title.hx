9f2d5a834192a77d4ec8b9610ccf3c3e09666dfb
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2024-07-06 13:46:24 +0800

    Add files via upload

--- source/backend/VideoHandler_Title.hx
+++ source/backend/VideoHandler_Title.hx
@@ -10,189 +10,193 @@ import flixel.util.FlxColor;
 
 #if VIDEOS_ALLOWED
 #if hxCodec
-import hxcodec.VLCBitmap;
-#else
-import vlc.VLCBitmap;
+    #if (hxCodec >= "3.0.0") import hxcodec.
+    #else import vlc.VLCBitmap;
+    #end
 #end
 
-class VideoHandler_Title extends VLCBitmap {
-    public var canSkip:Bool = true;
-    public var canUseSound:Bool = true;
-    public var canUseAutoResize:Bool = true;
+class VideoHandler_Title extends VLCBitmap
+{
+	public var canSkip:Bool = true;
+	public var canUseSound:Bool = true;
+	public var canUseAutoResize:Bool = true;
 
-    public var openingCallback:Void->Void = null;
-    public var finishCallback:Void->Void = null;
+	public var openingCallback:Void->Void = null;
+	public var finishCallback:Void->Void = null;
 
-    private var pauseMusic:Bool = false;
+	private var pauseMusic:Bool = false;
 
-    public function new(IndexModifier:Int = 0):Void {
-        super();
+	public function new(IndexModifier:Int = 0):Void
+	{
+		super();
 
-        onOpening = onVLCOpening;
-        onEndReached = onVLCEndReached;
-        onEncounteredError = onVLCEncounteredError;
+		onOpening = onVLCOpening;
+		onEndReached = onVLCEndReached;
+		onEncounteredError = onVLCEncounteredError;
 
-        FlxG.addChildBelowMouse(this, IndexModifier);
-    }
+		FlxG.addChildBelowMouse(this, IndexModifier);
+	}
 
-    private function onVLCOpening():Void {        
-        trace("the video is opening!");
-        if (openingCallback != null) {
-            openingCallback();
-        }
-    }
+	private function onVLCOpening():Void 
+	{        
+		trace("the video is opening!");
+		if (openingCallback != null)
+		    openingCallback();
+	}
 
-    private function onVLCEncounteredError():Void {
-        Lib.application.window.alert('Error cannot be specified', "VLC Error!");
-        onVLCEndReached();
-    }
+	private function onVLCEncounteredError():Void
+	{
+		Lib.application.window.alert('Error cannot be specified', "VLC Error!");
+		onVLCEndReached();
+	}
 
-    private function onVLCEndReached():Void {
-        if (FlxG.sound.music != null && pauseMusic) {
-            FlxG.sound.music.resume();
-        }
+	private function onVLCEndReached():Void
+	{
+		if (FlxG.sound.music != null && pauseMusic)
+			FlxG.sound.music.resume();
 
-        if (FlxG.stage.hasEventListener(Event.ENTER_FRAME)) {
-            FlxG.stage.removeEventListener(Event.ENTER_FRAME, update);
-        }
+		if (FlxG.stage.hasEventListener(Event.ENTER_FRAME))
+			FlxG.stage.removeEventListener(Event.ENTER_FRAME, update);
 
-        if (FlxG.autoPause) {
-            if (FlxG.signals.focusGained.has(resume)) {
-                FlxG.signals.focusGained.remove(resume);
-            }
+		if (FlxG.autoPause)
+		{
+			if (FlxG.signals.focusGained.has(resume))
+				FlxG.signals.focusGained.remove(resume);
 
-            if (FlxG.signals.focusLost.has(pause)) {
-                FlxG.signals.focusLost.remove(pause);
-            }
-        }
+			if (FlxG.signals.focusLost.has(pause))
+				FlxG.signals.focusLost.remove(pause);
+		}
 
-        dispose();
-        FlxG.removeChild(this);
+		dispose();
 
-        if (finishCallback != null) {
-            finishCallback();
-        }
-    }
+		FlxG.removeChild(this);
 
-    /**
-     * Plays a video.
-     *
-     * @param Path Example: `your/video/here.mp4`
-     * @param Loop Loop the video.
-     * @param PauseMusic Pause music until the video ends.
-     * @param Width Video width.
-     * @param Height Video height.
-     */
-    public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false, Width:Int, Height:Int):Void {
-        pauseMusic = PauseMusic;
+		if (finishCallback != null)
+			finishCallback();
+	}
 
-        if (FlxG.sound.music != null && PauseMusic) {
-            FlxG.sound.music.pause();
-        }
+	/**
+	 * Plays a video.
+	 *
+	 * @param Path Example: `your/video/here.mp4`
+	 * @param Loop Loop the video.
+	 * @param PauseMusic Pause music until the video ends.
+	 */
+	public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false, Width:Int, Height:Int):Void
+	{
+		pauseMusic = PauseMusic;
 
-        FlxG.stage.addEventListener(Event.ENTER_FRAME, update);
+		if (FlxG.sound.music != null && PauseMusic)
+			FlxG.sound.music.pause();
 
-        if (FlxG.autoPause) {
-            FlxG.signals.focusGained.add(resume);
-            FlxG.signals.focusLost.add(pause);
-        }
+		FlxG.stage.addEventListener(Event.ENTER_FRAME, update);
 
-        // In case you want to use another dir than the application one.
-        // Android can already do this, it can't use application's storage.
-        if (FileSystem.exists(Sys.getCwd() + Path)) {
-            play(Sys.getCwd() + Path, Loop);
-        } else {
-            play(Path, Loop);
-        }
-    }
+		if (FlxG.autoPause)
+		{
+			FlxG.signals.focusGained.add(resume);
+			FlxG.signals.focusLost.add(pause);
+		}
 
-    private function update(?E:Event):Void {
-        if (canSkip && (FlxG.keys.justPressed.ENTER #if android || FlxG.android.justReleased.BACK #end) && (isPlaying && isDisplaying)) {
-            onVLCEndReached();
-        }
+		// in case if you want to use another dir then the application one.
+		// android can already do this, it can't use application's storage.
+		if (FileSystem.exists(Sys.getCwd() + Path))
+			play(Sys.getCwd() + Path, Loop);
+		else
+			play(Path, Loop);
+	}
 
-        if (canUseAutoResize && (videoWidth > 0 && videoHeight > 0)) {
-            width = calcSize(0);
-            height = calcSize(1);
-        }
+	private function update(?E:Event):Void
+	{
+		if (canSkip && (FlxG.keys.justPressed.ENTER #if android || FlxG.android.justReleased.BACK #end) && (isPlaying && isDisplaying))
+			onVLCEndReached();
 
-        volume = #if FLX_SOUND_SYSTEM Std.int(((FlxG.sound.muted || !canUseSound) ? 0 : 1) * (FlxG.sound.volume * 100)) #else FlxG.sound.volume * 100 #end;
-    }
+		if (canUseAutoResize && (videoWidth > 0 && videoHeight > 0))
+		{
+			width = calcSize(0);
+			height = calcSize(1);
+		}
 
-    public function calcSize(Ind:Int):Int {
-        var appliedWidth:Float = Lib.current.stage.stageHeight * (FlxG.width / FlxG.height);
-        var appliedHeight:Float = Lib.current.stage.stageWidth * (FlxG.height / FlxG.width);
+		volume = #if FLX_SOUND_SYSTEM Std.int(((FlxG.sound.muted || !canUseSound) ? 0 : 1) * (FlxG.sound.volume * 100)) #else FlxG.sound.volume * 100 #end;
+	}
 
-        if (appliedHeight > Lib.current.stage.stageHeight) {
-            appliedHeight = Lib.current.stage.stageHeight;
-        }
+	public function calcSize(Ind:Int):Int
+	{
+		var appliedWidth:Float = Lib.current.stage.stageHeight * (FlxG.width / FlxG.height);
+		var appliedHeight:Float = Lib.current.stage.stageWidth * (FlxG.height / FlxG.width);
 
-        if (appliedWidth > Lib.current.stage.stageWidth) {
-            appliedWidth = Lib.current.stage.stageWidth;
-        }
+		if (appliedHeight > Lib.current.stage.stageHeight)
+			appliedHeight = Lib.current.stage.stageHeight;
 
-        switch (Ind) {
-            case 0:
-                return Std.int(appliedWidth);
-            case 1:
-                return Std.int(appliedHeight);
-        }
+		if (appliedWidth > Lib.current.stage.stageWidth)
+			appliedWidth = Lib.current.stage.stageWidth;
 
-        return 0;
-    }
+		switch (Ind)
+		{
+			case 0:
+				return Std.int(appliedWidth);
+			case 1:
+				return Std.int(appliedHeight);
+		}
+
+		return 0;
+	}
 }
 
-class VideoSprite extends FlxSprite {
-    public var bitmap:VideoHandler_Title;
-    public var openingCallback:Void->Void = null;
-    public var finishCallback:Void->Void = null;
-
-    public var newWidth = 0;
+class VideoSprite extends FlxSprite
+{
+	public var bitmap:VideoHandler_Title;
+	public var openingCallback:Void->Void = null;
+	public var finishCallback:Void->Void = null;
+	
+	public var newWidth = 0;
     public var newHeight = 0;
-
-    public function new(X:Float = 0, Y:Float = 0, Width:Int = 1280, Height:Int = 720) {
-        super(X, Y);
-
-        newWidth = Width;
+    
+	public function new(X:Float = 0, Y:Float = 0, Width:Int = 1280, Height:Int = 720)
+	{
+		super(X, Y);
+		
+		newWidth = Width;
         newHeight = Height;
+        
+		makeGraphic(1, 1, FlxColor.TRANSPARENT);
 
-        makeGraphic(1, 1, FlxColor.TRANSPARENT);
+		bitmap = new VideoHandler_Title();		
+		bitmap.alpha = 0;
+		bitmap.openingCallback = function()
+		{
+			if (openingCallback != null)
+				openingCallback();
+		}
+		bitmap.finishCallback = function()
+		{
+			if (finishCallback != null)
+				finishCallback();
 
-        bitmap = new VideoHandler_Title();        
-        bitmap.alpha = 0;
-        bitmap.openingCallback = function() {
-            if (openingCallback != null) {
-                openingCallback();
-            }
-        }
-        bitmap.finishCallback = function() {
-            if (finishCallback != null) {
-                finishCallback();
-            }
-            kill();
-        }
-    }
+			kill();
+		}
+	}
 
-    override function update(elapsed:Float) {
-        super.update(elapsed);
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
 
-        if (bitmap.isPlaying && bitmap.isDisplaying && bitmap.bitmapData != null) {
-            pixels = bitmap.bitmapData;
+		if (bitmap.isPlaying && bitmap.isDisplaying && bitmap.bitmapData != null){
+			pixels = bitmap.bitmapData;
 
-            var size:Float = Math.min(newWidth / bitmap.bitmapData.width, newHeight / bitmap.bitmapData.height);
-            scale.set(size, size); // lol    
-            updateHitbox();
-        }    
-    }
+		var size:Float = Math.min(newWidth / bitmap.bitmapData.width, newHeight / bitmap.bitmapData.height);
+		scale.set(size, size); // lol	
+		updateHitbox();
+		}	
+	}
 
-    /**
-     * Native video support for Flixel & OpenFL
-     * @param Path Example: `your/video/here.mp4`
-     * @param Loop Loop the video.
-     * @param PauseMusic Pause music until the video ends.
-     */
-    public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false):Void {
-        bitmap.playVideo(Path, Loop, PauseMusic, newWidth, newHeight);                
-    }
+	/**
+	 * Native video support for Flixel & OpenFL
+	 * @param Path Example: `your/video/here.mp4`
+	 * @param Loop Loop the video.
+	 * @param PauseMusic Pause music until the video ends.
+	 */
+	public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false):Void{
+		bitmap.playVideo(Path, Loop, PauseMusic, newWidth, newHeight);				
+	}
 }
 #end
