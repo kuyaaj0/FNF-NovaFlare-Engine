946ba0d41ce9a6405b58b08fec3e5c3bd5e56e6d
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2025-02-01 07:59:56 +0800

    Add files via upload

--- source/flixel/addons/ui/QuadMotion.hx
+++ source/flixel/addons/ui/QuadMotion.hx
@@ -3,7 +3,7 @@ package flixel.addons.ui;
 import flixel.FlxObject;
 import flixel.tweens.FlxTween;
 import flixel.tweens.motion.Motion;
-import flixel.tweens.EaseFunction;
+import flixel.util.FlxMath;
 
 class QuadMotion extends Motion {
     public var startX:Float;
@@ -11,28 +11,27 @@ class QuadMotion extends Motion {
     public var endX:Float;
     public var endY:Float;
     public var target:FlxObject;
-    public var duration:Float;
 
     public function new(target:FlxObject, startX:Float, startY:Float, endX:Float, endY:Float, duration:Float) {
-        // Set up the target and duration for the motion
+        // Set up the target and other parameters
         this.target = target;
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.duration = duration;
         
-        // Create a tween for the motion (using a quadratic ease function)
+        // Create a tween for the motion
         FlxTween.tween(target, {x: endX, y: endY}, duration, {
-            type: FlxTween.PERSIST,
-            ease: EaseFunction.QUAD_OUT, // Apply quadratic easing
+            type: FlxTween.PERSIST, // Allow tween to persist
             onUpdate: function(tween:FlxTween) {
-                var progress = tween.progress;
-                var curve = Math.pow(progress, 2); // Quadratic easing calculation
+                var progress = tween.progress; // Get the progress of the tween
 
-                // Update the target's position based on the easing
-                target.x = startX + (endX - startX) * curve;
-                target.y = startY + (endY - startY) * curve;
+                // Apply quadratic easing manually: Quadratic ease out
+                var easedProgress = 1 - Math.pow(1 - progress, 2);
+
+                // Update the position based on eased progress
+                target.x = startX + (endX - startX) * easedProgress;
+                target.y = startY + (endY - startY) * easedProgress;
             }
         });
     }
