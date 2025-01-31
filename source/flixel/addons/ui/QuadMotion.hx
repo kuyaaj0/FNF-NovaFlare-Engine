eb3cfd4046995abc994829cf0d720ee9c80aed56
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2025-02-01 07:29:47 +0800

    Add files via upload

--- source/flixel/addons/ui/QuadMotion.hx
+++ source/flixel/addons/ui/QuadMotion.hx
@@ -1,36 +1,41 @@
 package flixel.addons.ui;
 
-import flixel.tweens.FlxTween;
 import flixel.FlxObject;
+import flixel.tweens.FlxTween;
 import flixel.tweens.motion.Motion;
+import flixel.tweens.TweenOptions;
 
-class QuadMotion extends FlxTween {
+class QuadMotion extends Motion {
     public var startX:Float;
     public var startY:Float;
     public var endX:Float;
     public var endY:Float;
     public var target:FlxObject;
-    public var duration:Float;
 
     public function new(target:FlxObject, startX:Float, startY:Float, endX:Float, endY:Float, duration:Float) {
-        // Calling the super constructor with required arguments for FlxTween
-        super(target, { x: endX, y: endY }, duration);
+        // Create options for FlxTween with the target, duration, and easing function
+        var options = new TweenOptions();
+        options.x = endX;
+        options.y = endY;
+        options.duration = duration;
+        options.ease = Math.pow; // This is a simple example of quadratic easing
 
-        // Initializing the specific properties
+        // Calling FlxTween constructor with the options
+        super(target, options);
+
+        // Set additional properties
         this.target = target;
         this.startX = startX;
         this.startY = startY;
         this.endX = endX;
         this.endY = endY;
-        this.duration = duration;
     }
 
     override public function update(elapsed:Float):Void {
-        // Ensure the tween is progressing correctly
-        super.update(elapsed);
+        super.update(elapsed); // Update parent logic
 
-        // Get progress of motion (from 0 to 1)
-        var progress = this.progress;
+        // Get progress from the super class
+        var progress = super.progress;
         if (progress > 1) progress = 1; // Clamping to 1
 
         // Apply quadratic easing (easeInOut)
@@ -39,10 +44,5 @@ class QuadMotion extends FlxTween {
         // Update position using quadratic easing
         target.x = startX + (endX - startX) * curve;
         target.y = startY + (endY - startY) * curve;
-
-        // Optionally, you can add any other logic to trigger onComplete once done
-        if (progress == 1) {
-            // On completion actions (if any)
-        }
     }
 }
