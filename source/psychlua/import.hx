f0dbbcc54e78714440fc52a63eac7596b3edbe6c
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2025-02-06 13:33:32 +0800

    Update import.hx

--- source/psychlua/import.hx
+++ source/psychlua/import.hx
@@ -2,21 +2,3 @@
 import llua.*;
 import llua.Lua;
 #end
-
-import psychlua.*;
-import psychlua.Modifier;
-
-import math.Vector3;
-import math.VectorHelpers;
-
-import flixel.FlxSprite;
-import flixel.math.FlxAngle;
-import flixel.math.FlxPoint;
-import flixel.math.FlxMath;
-import flixel.math.FlxMath.lerp;
-
-import objects.Note;
-import objects.NoteSplash;
-import objects.StrumNote;
-
-import objects.playfields.NoteField;
