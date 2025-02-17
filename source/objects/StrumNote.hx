4f70792b6f6ef93597d832d5e9838415ba5c6746
kuyaaj0 <83687427+kuyaaj0@users.noreply.github.com>
2025-02-18 07:18:06 +0800

    Update StrumNote.hx

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -59,7 +59,7 @@ class StrumNote extends NoteObject
 		noteData = leColumn;
 		field = playField;
 		this.player = player;
-		this.objType = STRUM
+		this.objType = STRUM;
 		this.noteData = leColumn;
 		this.field = playField;
 		super(x, y);
